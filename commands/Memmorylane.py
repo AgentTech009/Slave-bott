1d840dd33ad4ffc9f6f973efcfde4f20057fe656
AgentTech009 <86217685+AgentTech009@users.noreply.github.com>
2026-05-04 00:56:03 +0530

    Update Memmorylane.py

--- commands/Memmorylane.py
+++ commands/Memmorylane.py
@@ -6,10 +6,12 @@ import random
 GIRL_NAME = "linet_sara_"
 BOY_NAME = "kokachi._"
 NARRATOR_NAME = "Narrator"
+FINAL_BOY_NAME = "niranjannnnnn.____"
 
 GIRL_PFP = "https://cdn.discordapp.com/attachments/1500472877210927197/1500573861866115234/IMG_20260504_003425.jpg?ex=69f8edcf&is=69f79c4f&hm=baeaf89fd0d95b2b8d19fe4af0397015ee82ddc409586d0ab3a11ae93f2e3c53&"
 BOY_PFP = "https://cdn.discordapp.com/attachments/1500472877210927197/1500573862146867391/50d7438ff1dd2822ed0cf84e7aaf4965.jpg?ex=69f8edd0&is=69f79c50&hm=57ee1f5c8cbba303bd4df99f5b7cb178d83b5653890b05da29bda600b95b259a&"
 NARRATOR_PFP = "https://cdn-icons-png.flaticon.com/512/833/833472.png"
+FINAL_BOY_PFP = "https://cdn.discordapp.com/attachments/1500472877210927197/1500578549000700038/533388631_18070949531471399_510634509294917079_n.jpg?ex=69f8f22d&is=69f7a0ad&hm=b426d103eda23a1de09a3070dbe2aa0c5b801af90576d2061c47e98e4f23430b&"
 
 
 class MemoryLane(commands.Cog):
@@ -89,6 +91,7 @@ class MemoryLane(commands.Cog):
         boy_webhook = await channel.create_webhook(name=BOY_NAME)
         girl_webhook = await channel.create_webhook(name=GIRL_NAME)
         narrator_webhook = await channel.create_webhook(name=NARRATOR_NAME)
+        final_webhook = await channel.create_webhook(name=FINAL_BOY_NAME)
 
         await asyncio.sleep(5)
 
@@ -101,7 +104,7 @@ class MemoryLane(commands.Cog):
             (GIRL_NAME, girl_webhook, "Aahhh aloichuuu...", GIRL_PFP, False),
             (BOY_NAME, boy_webhook, "Enthaaaa👀", BOY_PFP, False),
 
-            (NARRATOR_NAME, narrator_webhook, "**Girl goes to eat porotta while boy is dying out there**", NARRATOR_PFP, True),
+            (NARRATOR_NAME, narrator_webhook, "**Girl eating porotta while boy is dying out there**", NARRATOR_PFP, True),
             (NARRATOR_NAME, narrator_webhook, "**15 minutes later**", NARRATOR_PFP, True),
 
             (GIRL_NAME, girl_webhook, "Aahhh athhhh", GIRL_PFP, False),
@@ -122,21 +125,32 @@ class MemoryLane(commands.Cog):
             ),
 
             (
-                BOY_NAME,
-                boy_webhook,
+                NARRATOR_NAME,
+                narrator_webhook,
+                "*someone is typing...*",
+                NARRATOR_PFP,
+                True
+            ),
+
+            (
+                FINAL_BOY_NAME,
+                final_webhook,
                 "Babyy..\n"
                 "I will always be with you🥹🥹\n"
                 "I WILL ALWAYS LOVE YOU🥹🥹\n"
                 "-your nervous little boy..",
-                BOY_PFP,
+                FINAL_BOY_PFP,
                 False
             )
         ]
 
         for name, webhook, message, pfp, narrator in script:
+            if message == "Babyy..\nI will always be with you🥹🥹\nI WILL ALWAYS LOVE YOU🥹🥹\n-your nervous little boy..":
+                await asyncio.sleep(3)
+
             await self.send_as(channel, webhook, guild, name, message, pfp, narrator)
 
-        for webhook in [boy_webhook, girl_webhook, narrator_webhook]:
+        for webhook in [boy_webhook, girl_webhook, narrator_webhook, final_webhook]:
             try:
                 await webhook.delete()
             except:
